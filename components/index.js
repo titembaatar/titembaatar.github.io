11021c062868d49f084788bb79544291f9a39141
titembaatar <53315929+titembaatar@users.noreply.github.com>
2021-10-13 11:01:35 +0900

    nuxt

--- components/index.js
+++
@@ -1,7 +0,0 @@
-import { wrapFunctional } from './utils'
-
-export { default as ButtonGroup } from '../..\\components\\buttonGroup.vue'
-export { default as DropdownMenu } from '../..\\components\\DropdownMenu.vue'
-export { default as IconLogo } from '../..\\components\\iconLogo.vue'
-export { default as SvgPattern } from '../..\\components\\svgPattern.vue'
-export { default as SvgPocket } from '../..\\components\\svgPocket.vue'
